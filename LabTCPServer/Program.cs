b8ea1e4ee705a11de7df4cea9dad951f9bbea1f3
ba-trungtq <trungtq@binhanh.com>
2017-06-05 14:28:21 +0700

    Add project files.

---
+++ LabTCPServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start TCP Server
+            TCPServer.Run();
+        }
+    }
+}
---
+++ LabTCPServer/TCPServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LabTCPServer
+{
+    public class TCPServer
+    {
+        private const int BUFFER_SIZE = 1024;
+        private const int PORT_NUMBER = 9999;
+
+        static ASCIIEncoding encoding = new ASCIIEncoding();
+
+        public static void Run()
+        {
+            try
+            {
+                IPAddress address = IPAddress.Parse("127.0.0.1");
+
+                TcpListener listener = new TcpListener(address, PORT_NUMBER);
+
+                // 1. Listener
+                listener.Start();
+
+                Console.WriteLine("Server start on: {0}", listener.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection...");
+
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("Connection receiver from: {0}", socket.RemoteEndPoint);
+
+                // 2. Receiver
+                byte[] data = new byte[BUFFER_SIZE];
+                socket.Receive(data);
+
+                string str = encoding.GetString(data);
+
+                // 3. Send
+                socket.Send(encoding.GetBytes("Hello " + str));
+
+                // 4. Close
+                socket.Close();
+                listener.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
